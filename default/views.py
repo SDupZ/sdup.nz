cb41c102a41ee843a08feb3e3dc059ae1af48b42
Simon du Preez <smdupnz@yahoo.co.nz>
2017-04-07 00:58:28 +1200

    Added jumbrotron image sitesettings

--- default/views.py
+++ default/views.py
@@ -1,12 +1,18 @@
 from django.shortcuts import render
 from projects.models import Project
-
+from site_settings.models import HomepageSettings
 
 def home(request):
     template = 'index.html'
     projects = Project.objects.all()
 
+    homepage_settings = HomepageSettings.objects.all()
+
+    if len(homepage_settings) > 0:
+        homepage_settings = homepage_settings[0]
+
     context = {
         "projects": projects,
+        "homepage_settings": homepage_settings
     }
     return render(request, template, context)
